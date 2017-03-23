2ab030621060ac9d40dd966c9a679656d57dae78
Disorder <disorrder@gmail.com>
2017-03-23 19:10:57 +0300

    add last vk messages

--- vk/index.js
+++ vk/index.js
@@ -4,6 +4,10 @@ const passport = require('koa-passport');
 const Router = require('koa-router');
 var router = new Router();
 
+const VK = require('./api');
+var vk = new VK(cfg);
+vk.access_token = '4c9b5cf60a1151e555ae8a23362ffbec92e4511cb2cfa93160e6f99e842d1a159f78f78cdb1d02c61cbb0';
+
 router.get('/', (ctx) => {
     console.log(ctx);
     ctx.body = "VK";
@@ -21,5 +25,10 @@ router.get('/auth/callback', passport.authenticate('vkontakte', {
     successRedirect: '/bot',
     failureRedirect: '/vk?status=failed'
 }));
+
+router.get('/messages', async ctx => {
+    var res = await vk.method('messages.get', {count: ctx.query.count});
+    ctx.body = res;
+});
 
 module.exports = router;
